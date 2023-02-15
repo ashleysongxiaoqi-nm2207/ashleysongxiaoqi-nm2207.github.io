53c164579ac5db4314ad65a7417812e7461f6046
ashleysongxiaoqi-nm2207 <125440595+ashleysongxiaoqi-nm2207@users.noreply.github.com>
2023-02-15 16:37:28 +0800

    Add files via upload

--- appscripts/main.js
+++ appscripts/main.js
@@ -1,12 +1,12 @@
 // main.js
 
-console.log(`yo`);
 
-let divArray = document.body.getElementsByTagName("div");
-for(el of divArray)
-{
-    console.log(el.innerHTML);
-}
+// Hello only appears once because the function is executed only when it is called and it is only called once.
 
-console.log(divArray[3].innerHTML);
-divArray[4].innerHTML = "New text";
+// Track the number of clicks. It can do this by increasing the value of the counter variable that you set in Step 1e, by 1.
+
+
+// Change the text in the header element to read something like "OK, I have now received X clicks" (where X is the click count). So comment out the command for Step 1f and write a new one to achieve this instead.
+
+
+// Now make it lie: have it print out 100 times the number of actual clicks it has received.
